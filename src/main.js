02530445232f56281d82a3886ab1489fe35e33d6
Yoshihiro Tanaka <contact@cordea.jp>
2023-06-04 15:37:51 +0900

    reformat with prettier

--- src/main.js
+++ src/main.js
@@ -1,30 +1,28 @@
 import * as core from '@actions/core'
-import {Youtrack} from "youtrack-rest-client";
-import {context} from "@actions/github";
+import { Youtrack } from 'youtrack-rest-client'
+import { context } from '@actions/github'
 
-const AsyncFunction = async function () {
-}.constructor;
+const AsyncFunction = async function () {}.constructor
 
 async function main() {
-    const token = core.getInput('youtrack-token', {required: true})
-    const url = core.getInput('youtrack-url', {required: true})
-    const script = core.getInput('script', {required: true})
-    const encoding = core.getInput('result-encoding')
+  const token = core.getInput('youtrack-token', { required: true })
+  const url = core.getInput('youtrack-url', { required: true })
+  const script = core.getInput('script', { required: true })
+  const encoding = core.getInput('result-encoding')
 
-    const client = new Youtrack({
-        baseUrl: url,
-        token: token
-    })
-    const result = await new AsyncFunction(
-        'context',
-        'core',
-        'youtrack',
-        script
-    )(context, core, client)
+  const client = new Youtrack({
+    baseUrl: url,
+    token: token
+  })
+  const result = await new AsyncFunction('context', 'core', 'youtrack', script)(
+    context,
+    core,
+    client
+  )
 
-    core.setOutput('result', result)
+  core.setOutput('result', result)
 }
 
 main().catch((e) => {
-    core.setFailed(`Got an unhandled error. ${e}`)
+  core.setFailed(`Got an unhandled error. ${e}`)
 })
