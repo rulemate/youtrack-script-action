6f37be6693b14376d8787c87e858ad221214f876
Yoshihiro Tanaka <contact@cordea.jp>
2023-06-04 13:16:17 +0900

    initialize the client

--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
-async function main() {
+import * as core from '@actions/core'
+import {Youtrack} from "youtrack-rest-client";
 
+async function main() {
+    const token = core.getInput('youtrack-token', {required: true})
+    const url = core.getInput('youtrack-url', {required: true})
+    const script = core.getInput('script', {required: true})
+    const encoding = core.getInput('result-encoding')
+
+    const client = new Youtrack({
+        baseUrl: url,
+        token: token
+    })
 }
+
+main().catch((e) => {
+    core.setFailed(`Got an unhandled error. ${e}`)
+})
